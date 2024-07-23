ccbe8b40530a5dada0095cb0e204711c718ac968
Sumi <sumilb@alumni.mioti.es>
2024-07-23 16:26:06 +0200

    Update script with correct endpoint and space name

--- scripts/upload_to_spaces.py
+++ scripts/upload_to_spaces.py
@@ -5,12 +5,12 @@ import os
 def upload_to_spaces():
     session = boto3.session.Session()
     client = session.client('s3',
-                            region_name='nyc3',
-                            endpoint_url='https://nyc3.digitaloceanspaces.com',
+                            region_name='fra1',
+                            endpoint_url='https://climaplatano.fra1.digitaloceanspaces.com',
                             aws_access_key_id=os.environ['SPACES_ACCESS_KEY_ID'],
                             aws_secret_access_key=os.environ['SPACES_SECRET_ACCESS_KEY'])
 
-    space_name = 'climaplatano'
+    space_name = 'climaplatano'  # Asegúrate de que esto sea correcto
 
     client.upload_file('base_de_datos_clima_cleaned.csv', space_name, 'base_de_datos_clima_cleaned.csv')
     client.upload_file('consolidado_agricola_cleaned.csv', space_name, 'consolidado_agricola_cleaned.csv')
