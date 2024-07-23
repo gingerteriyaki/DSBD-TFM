1d3e9b8c6747462b66c1b232873b08455a822321
Sumi <sumilb@alumni.mioti.es>
2024-07-23 16:13:03 +0200

    Fixed code to access DigitalOceans

--- scripts/upload_to_spaces.py
+++ scripts/upload_to_spaces.py
@@ -1,13 +1,14 @@
 import boto3
 from botocore.client import Config
+import os
 
 def upload_to_spaces():
     session = boto3.session.Session()
     client = session.client('s3',
                             region_name='nyc3',
                             endpoint_url='https://nyc3.digitaloceanspaces.com',
-                            aws_access_key_id='your-access-key-id',
-                            aws_secret_access_key='your-secret-access-key')
+                            aws_access_key_id=os.environ['SPACES_ACCESS_KEY_ID'],
+                            aws_secret_access_key=os.environ['SPACES_SECRET_ACCESS_KEY'])
 
     client.upload_file('base_de_datos_clima_cleaned.csv', 'your-space-name', 'base_de_datos_clima_cleaned.csv')
     client.upload_file('consolidado_agricola_cleaned.csv', 'your-space-name', 'consolidado_agricola_cleaned.csv')
