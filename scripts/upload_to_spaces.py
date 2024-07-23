6d33f81e61594a9b947dc0c6b8551fba6c206a6a
Sumi <sumilb@alumni.mioti.es>
2024-07-23 16:20:52 +0200

    Fixed code to access DigitalOceans Space

--- scripts/upload_to_spaces.py
+++ scripts/upload_to_spaces.py
@@ -10,8 +10,10 @@ def upload_to_spaces():
                             aws_access_key_id=os.environ['SPACES_ACCESS_KEY_ID'],
                             aws_secret_access_key=os.environ['SPACES_SECRET_ACCESS_KEY'])
 
-    client.upload_file('base_de_datos_clima_cleaned.csv', 'your-space-name', 'base_de_datos_clima_cleaned.csv')
-    client.upload_file('consolidado_agricola_cleaned.csv', 'your-space-name', 'consolidado_agricola_cleaned.csv')
+    space_name = 'climaplatano'
+
+    client.upload_file('base_de_datos_clima_cleaned.csv', space_name, 'base_de_datos_clima_cleaned.csv')
+    client.upload_file('consolidado_agricola_cleaned.csv', space_name, 'consolidado_agricola_cleaned.csv')
     print("Datos subidos a DigitalOcean Spaces.")
 
 if __name__ == "__main__":
