287b0af61e5237194ceddd19e8a55ae4b681c341
Sumi <sumilb@mioti.es>
2024-07-28 19:16:09 +0200

    Actualizacion VS Code

--- Desarrollo/test.py
+++ Desarrollo/test.py
@@ -1,2 +1,11 @@
-print("hola")
+import sys
+import os
 
+# Añadir el directorio 'scripts' al PYTHONPATH
+sys.path.append(os.path.join(os.path.dirname(__file__), 'scripts'))
+
+# Ahora puedes importar el módulo
+from download_data import download_files
+
+# Llamar a la función para descargar los archivos
+download_files()
