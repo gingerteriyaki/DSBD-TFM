3fcd9a2aeddf25d8ce87bc57c13469af39c6ac28
Sumi <sumilb@alumni.mioti.es>
2024-07-29 18:17:28 +0200

    Configuracion app.py para API

--- app.py
+++ app.py
@@ -1,33 +1,52 @@
 from flask import Flask, jsonify
 import boto3
+import pandas as pd
 import os
 
 app = Flask(__name__)
 
+# Configurar el cliente de S3
 session = boto3.session.Session()
-    client = session.client('s3',
-                            region_name='fra1',
-                            endpoint_url='https://climaplatano.fra1.digitaloceanspaces.com',
-                            aws_access_key_id=os.environ['SPACES_ACCESS_KEY_ID'],
-                            aws_secret_access_key=os.environ['SPACES_SECRET_ACCESS_KEY'])
+client = session.client('s3',
+                        region_name='fra1',
+                        endpoint_url='https://climaplatano.fra1.digitaloceanspaces.com',
+                        aws_access_key_id=os.environ['SPACES_ACCESS_KEY_ID'],
+                        aws_secret_access_key=os.environ['SPACES_SECRET_ACCESS_KEY'])
 
-    space_name = 'climaplatano'  # Cambia esto por el nombre de tu Space en DigitalOcean
+bucket_name = 'climaplatano'
+file_names = {
+    'data_agricola': 'data_agricola.xlsx',
+    'datos_climaticos': 'datos_climaticos.xlsx',
+    # Agrega más archivos según sea necesario
+}
 
-def get_file_from_spaces(file_name):
-    try:
-        response = client.get_object(Bucket=bucket_name, Key=file_name)
-        return response['Body'].read().decode('utf-8')
-    except Exception as e:
-        print(f"Error al obtener el archivo: {e}")
-        return None
+@app.route('/data_agricola', methods=['GET'])
+def obtener_datos_mensuales():
+    # Descargar el archivo desde DigitalOcean Spaces
+    client.download_file(bucket_name, file_names['data_agricola'], 'data_agricola.xlsx')
+    
+    # Leer el archivo Excel
+    df = pd.read_excel('data_agricola.xlsx')
+    
+    # Convertir a JSON
+    datos_json = df.to_json(orient='records')
+    
+    return jsonify(datos_json)
 
-@app.route('/data/<file_name>', methods=['GET'])
-def get_data(file_name):
-    data = get_file_from_spaces(file_name)
-    if data:
-        return jsonify({"data": data})
-    else:
-        return jsonify({"error": "File not found"}), 404
+@app.route('/datos_climaticos', methods=['GET'])
+def obtener_datos_climaticos():
+    # Descargar el archivo desde DigitalOcean Spaces
+    client.download_file(bucket_name, file_names['datos_climaticos'], 'datos_climaticos.xlsx')
+    
+    # Leer el archivo Excel
+    df = pd.read_excel('datos_climaticos.xlsx')
+    
+    # Convertir a JSON
+    datos_json = df.to_json(orient='records')
+    
+    return jsonify(datos_json)
+
+# Agrega más rutas para otros archivos si es necesario
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
